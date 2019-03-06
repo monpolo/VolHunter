a6af96aa6e4a26951dbe04c94c21a3fb9a793fbf
monpolo <39749344+monpolo@users.noreply.github.com>
2019-03-06 16:57:47 -0600

    For report generation v0.1
    Needs to add logic for checking if volatility output files exist in the gatheredlogs folder. For each cell, get name, check if this plugin exists and is not 0kb, color appropriately.

---
+++ EXCEL VALUE AND COLOR.ps1
@@ -0,0 +1,32 @@
+﻿$path = (pwd).Path
+$fullpath = $path + "\test.xlsx"
+
+$excelFile = $fullpath #'C:\Users\bob\Desktop\test.xlsx'
+$Excel = New-Object -ComObject Excel.Application
+$Excel.Visible = $false
+$Excel.DisplayAlerts = $false
+$wb = $Excel.Workbooks.Open($excelFile)
+[int]$i = 1
+$bob = ($wb.Sheets.Item("Sheet1").Cells.Item($i,1).Text)
+
+while($bob -notlike $null){
+    #$wb.Sheets.Item("Sheet1").Range("A$i").Interior.ColorIndex
+    #IF EXISTS and size > 0, green, else if exists & size==0 yellow, else red
+    ###SET TO RED
+    ###https://docs.microsoft.com/en-us/office/vba/api/Excel.ColorIndex
+    $sheet = $wb.Worksheets.Item(1)
+    $sheet.Cells.Item($i,2).Interior.ColorIndex = 3
+
+    $bob = ($wb.Sheets.Item("Sheet1").Cells.Item($i,1).Text)
+    #$bob
+    $bob = ($wb.Sheets.Item("Sheet1").Cells.Item(($i + 1),1).Text)
+    if($bob -like $null){ break}
+    $i++
+}#>
+
+$wb.SaveAs("$fullpath")
+$wb.Close()
+$Excel.Quit()
+
+
+
